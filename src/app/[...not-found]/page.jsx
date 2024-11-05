01644e8932a8ed76c07cb0f58105492a0ecdebcd
Surya <suryapratapbbr21@gmail.com>
2024-11-05 19:41:41 +0530

    Build fix (temp add runtime edge export)

--- src/app/[...not-found]/page.jsx
+++ src/app/[...not-found]/page.jsx
@@ -1,15 +1,15 @@
-import LayoutStyle7 from '@/components/Layouts/LayoutStyle7';
-import NotFoundContent from '@/components/NotFound/NotFoundContent';
-import React from 'react';
-
+import LayoutStyle7 from "@/components/Layouts/LayoutStyle7";
+import NotFoundContent from "@/components/NotFound/NotFoundContent";
+import React from "react";
+export const runtime = "edge";
 const NotFoundPage = () => {
-    return (
-        <>
-            <LayoutStyle7 breadCrumb="404" title="404 Page">
-                <NotFoundContent />
-            </LayoutStyle7>
-        </>
-    );
+  return (
+    <>
+      <LayoutStyle7 breadCrumb="404" title="404 Page">
+        <NotFoundContent />
+      </LayoutStyle7>
+    </>
+  );
 };
 
 export default NotFoundPage;
